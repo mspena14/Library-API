cec925b4349592fc735b0184ba3b534249040eef
mspena14 <marlono1naranjo@gmail.com>
2024-09-28 11:06:21 -0500

    feat: updateBook endpoint created

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { Book } from './entities/book.entity';
 import { FindAllBooksParamsDto } from './dto/find-all-books-params.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -21,4 +22,9 @@ export class BooksController {
   findOneBookById(@Param('id') id: number): Promise<Book> {
     return this.booksService.findOneBookById(id);
   }
+
+  @Patch(':id') 
+    updateBookById(@Param('id') id: number, @Body() updateBookData: UpdateBookDto): Promise<Book> {
+      return this.booksService.updateBookById(id, updateBookData);
+    }
 }
