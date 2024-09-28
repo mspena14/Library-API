b5cef00108900312f52f17fd47cc45125074eb65
mspena14 <marlono1naranjo@gmail.com>
2024-09-27 22:57:51 -0500

    fix: configurations prettier fixed for microsoft

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,4 +23,10 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
   },
+  "prettier/prettier": [
+    "error",
+    {
+      "endOfLine": "auto"
+    }
+  ],
 };
