9a44929f97fa8e5535e47645618ae1a2a7d6538c
mspena14 <marlono1naranjo@gmail.com>
2024-09-27 22:58:28 -0500

    feat: findAllBooks service created

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { Repository } from 'typeorm';
@@ -9,8 +9,14 @@ export class BooksService {
   constructor(
     @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
   ) {}
-  createBook(createBookData: CreateBookDto): Promise<CreateBookDto> {
+  async createBook(createBookData: CreateBookDto): Promise<CreateBookDto> {
     const bookCreated = this.bookRepository.create(createBookData);
-    return this.bookRepository.save(bookCreated);
+    return await this.bookRepository.save(bookCreated);
+  }
+
+  async findAllBooks(): Promise<Book[]>{
+    const books = await this.bookRepository.find()
+    if (books.length === 0) throw new NotFoundException("Books not found")
+    return books;
   }
 }
