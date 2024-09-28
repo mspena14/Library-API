a965d0f346ad0e682cbab5c83f503e30e836fdca
mspena14 <marlono1naranjo@gmail.com>
2024-09-27 19:46:44 -0500

    feat: Create-book.dto  implemented in the controller and service of books

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { Repository } from 'typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
   constructor(
     @InjectRepository(Book) private readonly bookRepository: Repository<Book>,
   ) {}
-  createBook(): {
-    return 'This action adds a new book';
+  createBook(createBookData: CreateBookDto): Promise<CreateBookDto> {
+    const bookCreated = this.bookRepository.create(createBookData);
+    return this.bookRepository.save(bookCreated);
   }
 }
