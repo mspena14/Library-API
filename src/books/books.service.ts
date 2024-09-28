64a52d1d3a2a1853f9bd4409420fee1f35149e4b
mspena14 <marlono1naranjo@gmail.com>
2024-09-28 08:20:59 -0500

    feat: filters function added

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { CreateBookDto } from './dto/create-book.dto';
+import { FindAllBooksParamsDto } from './dto/find-all-books-params.dto';
+
 
 @Injectable()
 export class BooksService {
@@ -14,9 +16,27 @@ export class BooksService {
     return await this.bookRepository.save(bookCreated);
   }
 
-  async findAllBooks(): Promise<Book[]>{
-    const books = await this.bookRepository.find()
-    if (books.length === 0) throw new NotFoundException("Books not found")
+  async findAllBooks(params: FindAllBooksParamsDto): Promise<Book[]>{
+    const query = this.validateParamsForFindAll(params)
+    const books = await query.getMany();
+    if (books.length === 0) throw new NotFoundException("There are not books availables")
     return books;
+  }
+
+  validateParamsForFindAll(params: FindAllBooksParamsDto): SelectQueryBuilder<Book> {
+    const query = this.bookRepository.createQueryBuilder('book')
+    if (params.author) {
+      query.where('book.author = :author', { author: params.author });
+    }
+
+    if(params.genre) {
+      query.andWhere('book.genre = :genre', { genre: params.genre });
+    }
+
+    if (params.publicationDate) {
+      query.andWhere('book.publicationDate = :publicationDate', { publicationDate: params.publicationDate })
+    }
+
+    return query
   }
 }
